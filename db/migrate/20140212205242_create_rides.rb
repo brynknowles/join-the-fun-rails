9de3cf7c08213d31d559a99a94e2f9c8ea028bd3
brynknowles <bryn.knowles@gmail.com>
2020-10-08 20:45:41 -0400

    lab completed officially

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,8 +2,6 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
-      t.integer :passenger_id
-      t.integer :taxi_id
     end
   end
 end
